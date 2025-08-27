f6cb5b99ae71ac64f3d474613b510edf34384167
NazmuzShaad <nazmuzshaad@gmail.com>
2025-08-27 23:56:01 +0600

    Added copy Function

--- script.js
+++ script.js
@@ -11,8 +11,35 @@ for(let loveButton of loveButtons){
     })
 }
 
+// Copy Function 
+let copyCountEl=document.getElementById('copy-count');
+let copyCount= parseInt(copyCountEl.innerText) ;
+let copyButtons= document.getElementsByClassName('copy-button');
 
-document.getElementById('copy-button-1').addEventListener('click',function(){
-    const copyText  = document.getElementById('copy-text-1').innerText;
+for(let copyButton of copyButtons){
+    copyButton.addEventListener('click', function(){
+        copyCount++;
+        copyCountEl.innerText=copyCount;
+    })
+}
+
+function clickFunction(btnID , txtID){
+    document.getElementById(btnID).addEventListener('click',function(){
+    const copyText  = document.getElementById(txtID).innerText;
     navigator.clipboard.writeText(copyText);
 })
+}
+
+
+// Button-1 
+clickFunction('copy-button-1','copy-text-1');
+
+
+
+
+// Button-2 
+clickFunction('copy-button-2','copy-text-2')
+
+
+// Button-3 
+clickFunction('copy-button-3','copy-text-3')
